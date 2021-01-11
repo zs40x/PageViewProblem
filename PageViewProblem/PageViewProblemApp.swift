22e8fbc78efb5c1e851daca8a8ed760584a24cb1
Stefan Mehnert <s.mehnert@polipol.de>
2021-01-11 12:08:32 +0100

    Initial Commit

---
+++ PageViewProblem/PageViewProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageViewProblemApp.swift
+//  PageViewProblem
+//
+//  Created by Stefan Mehnert on 11.01.21.
+//
+
+import SwiftUI
+
+@main
+struct PageViewProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
