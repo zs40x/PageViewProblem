f8b89c77d644f5009b19f4f00cd54d2e54eeadfd
Stefan Mehnert <s.mehnert@polipol.de>
2021-01-11 13:30:28 +0100

    Reduced the number of the items in the tabView.

--- PageViewProblem/ContentView.swift
+++ PageViewProblem/ContentView.swift
@@ -35,11 +35,7 @@ class ContentViewModel: ObservableObject {
                 PageViewItem(id: 1, name: "Item 2"),
                 PageViewItem(id: 2, name: "Item 3"),
                 PageViewItem(id: 3, name: "Item 4"),
-                PageViewItem(id: 4, name: "Item 5"),
-                PageViewItem(id: 5, name: "Item 6"),
-                PageViewItem(id: 6, name: "Item 6"),
-                PageViewItem(id: 7, name: "Item 7"),
-                PageViewItem(id: 8, name: "Last Item"),
+                PageViewItem(id: 4, name: "Last Item"),
             ])
     }
 }
